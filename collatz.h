2eb2de976550389eb7ddb78e7c4f4614bb2059c6
Jericho0318 <111917174+Jericho0318@users.noreply.github.com>
2024-12-10 20:35:40 -0800

    Update collatz.h

--- collatz.h
+++ collatz.h
@@ -1,28 +1,25 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 #include "lrucache.h"
 
 uint64_t collatz(LRUCache* cache, uint64_t num) {
-    if (num <= 1) {
+    if (num <= 1)
         return num;
-    }
 
     uint64_t result = get(cache, num);
-    if (result != -1) {
+    if (result != -1)
         return result;
-    }
 
     uint64_t count = 0;
     uint64_t original_num = num;
 
     while (num != 1) {
-        if (num % 2 == 0) {
+        if (num % 2 == 0)
             num = num / 2;
-        } else {
+        else
             num = (3 * num) + 1;
-        }
         count++;
     }
 
